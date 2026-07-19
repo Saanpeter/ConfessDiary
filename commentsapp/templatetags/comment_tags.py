aa6ae60491f7783ea443aba93c9b453beebc8196
Saan peter <petersaan505@gmail.com>
2026-07-20 02:44:31 +0530

    Fix recursive comment rendering

--- commentsapp/templatetags/comment_tags.py
+++ commentsapp/templatetags/comment_tags.py
@@ -3,10 +3,14 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('commentsapp/comment_item.html', takes_context=True)
+@register.inclusion_tag(
+    'commentsapp/comment_item.html',
+    takes_context=True
+)
 def render_comment(context, comment, depth=0):
     return {
         'comment': comment,
         'depth': depth,
-        'user': context['user'],
+        'request': context.get('request'),
+        'user': context.get('user'),
     }
