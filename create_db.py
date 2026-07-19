84dc7f06464393b657d7cbd11c1afb1c2f446cb7
Saan peter <petersaan505@gmail.com>
2026-07-20 00:23:34 +0530

    Connect Django to Neon PostgreSQL

--- create_db.py
+++ create_db.py
@@ -1,15 +1,16 @@
-﻿import psycopg2
+﻿import os
+import psycopg2
 
 conn = psycopg2.connect(
-    dbname="postgres",
-    user="postgres",
-    password="735698.Sa",
-    host="localhost",
-    port="5432"
+    dbname=os.environ.get('DB_ADMIN_NAME', 'postgres'),
+    user=os.environ.get('DB_ADMIN_USER', 'postgres'),
+    password=os.environ.get('DB_ADMIN_PASSWORD', ''),
+    host=os.environ.get('DB_HOST', 'localhost'),
+    port=os.environ.get('DB_PORT', '5432')
 )
 conn.autocommit = True
 cur = conn.cursor()
-cur.execute("CREATE DATABASE whisperbook;")
+cur.execute('CREATE DATABASE whisperbook;')
 cur.close()
 conn.close()
-print("Database created.")
+print('Database created.')
