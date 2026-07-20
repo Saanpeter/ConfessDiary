0055602f996d2e1aab0fcd0c81d23a0487d542d1
Saan peter <petersaan505@gmail.com>
2026-07-20 12:10:12 +0530

    Switch to Resend for production email delivery

--- config/settings.py
+++ config/settings.py
@@ -198,6 +198,14 @@ SOCIALACCOUNT_PROVIDERS = {
         'AUTH_PARAMS': {'access_type': 'online'},
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.resend.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'resend'
+EMAIL_HOST_PASSWORD = os.environ.get('RESEND_API_KEY')
+DEFAULT_FROM_EMAIL = 'Whisperbook <onboarding@resend.dev>'
 # ----------------------------------------------------
 # Custom User Model
 # ----------------------------------------------------
